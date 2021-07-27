6c9a6ae81f0feaac6e4a2d9ac7ca9a7828dfa0ae
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2021-07-27 22:02:52 +0530

    =done front react

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -31,20 +31,47 @@ export default class App extends Component {
   inc = () => {
     this.setState({ count: this.state.count + 1 });
   };
+  newTodo = () => {
+    let name = prompt("Enter Name of new Todo");
+    let date = prompt("Enter Date of new Todo");
+    fetch(`http://localhost:8000/new/${name}/${date}`).then((res) => {
+      this.getData();
+    });
+  };
+  rmTodo = () => {
+    let {data}=this.state;
+    let index = prompt(
+      `Enter index of todo to be removed 0..${data.length}`
+    );
+    if (index != null) {
+      let indexi = parseInt(index);
+      if (indexi >= 0 && indexi < data.length) {
+        fetch(`http://localhost:8000/rm/${indexi}`).then((res) => {
+        });
+      }
+    }
+    this.getData();
+  };
   render() {
     console.log(this.state.data);
 
     return (
-      <div className="todo">
-        {this.state.data.map((todo) => {
-          return (
-            // <h1></h1>
-            <div>
-              <div>{todo.name}</div>
-              <div>{todo.date}</div>
-            </div>
-          );
-        })}
+      <div>
+        <div className="head">ToDo Handler</div>
+        <button onClick={this.newTodo}>New </button>
+        <button onClick={this.rmTodo}>Remove</button>
+        <div className="todo">
+          {this.state.data.map((todo, index) => {
+            return (
+              // <h1></h1>
+              <div key={index}>
+                <div>{index}</div>
+                <div>{todo.name}</div>
+                <div>{todo.date}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
